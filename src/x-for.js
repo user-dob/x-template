a8d93c08e62fc1d055c778e9a3cccd67870d1d4b
user-dob <user-dob@yandex.ua>
2014-11-11 17:58:42 +0200

    dfs

--- src/x-for.js
+++ src/x-for.js
@@ -1,14 +1,21 @@
+data = {
+    users: [
+        {name: 'user 1', age: 1},
+        {name: 'user 2', age: 1},
+        {name: 'user 3', age: 1},
+        {name: 'user 4', age: 1},
+        {name: 'user 5', age: 1}
+    ]
+};
+
+
 (function(window, document) {
 
     var element = Object.create(HTMLElement.prototype);
 
     element.createdCallback = function() {
         var el = this;
-
-        //console.log(this.childNodes)
-
-        //el.template = el.querySelector('template').content;
-        el.template = {}
+        el.template = el.querySelector('template').content;
     };
 
     element.attachedCallback = function() {
@@ -24,17 +31,33 @@
 
     element.compile = function() {
         var el = this;
-        if (el.hasAttribute('repeat')) {
-            var value = el.getAttribute('repeat');
-            var node = document.importNode(el.template, true);
-            var fragment = document.createDocumentFragment();
+        if (el.hasAttribute('condition')) {
+            var value = el.getAttribute('condition');
+            var itemName, itemsName;
 
-            for(var i=0; i<value; i++) {
-                fragment.appendChild(node.cloneNode(true));
-            };
+            value.replace(/\s*(\w+)\sin\s([\w\.]+)\s*/g, function(math, item, items) {
+                itemName = item;
+                itemsName = items;
+            });
+
+            var fragment = document.createDocumentFragment();
+            var node = document.importNode(el.template, true);
 
             el.clear();
             el.appendChild(fragment);
+
+            console.log(itemName, itemsName)
+
+
+//            var node = document.importNode(el.template, true);
+//            var fragment = document.createDocumentFragment();
+//
+//            for(var i=0; i<value; i++) {
+//                fragment.appendChild(node.cloneNode(true));
+//            };
+//
+//            el.clear();
+//            el.appendChild(fragment);
         };
     };
 
--- src/x-view.js
+++ src/x-view.js
@@ -3,17 +3,25 @@
     var element = Object.create(HTMLElement.prototype);
 
     element.createdCallback = function() {
-
-        //el.template = el.querySelector('template').content;
-
-        console.log( this.querySelector('template').content )
+        var el = this;
+        el.template = el.querySelector('template').content;
     };
 
     element.attachedCallback = function() {
-        console.log('attachedCallback')
+        var el = this;
+        el.clear();
+        el.appendChild(el.template);
+        console.log( el.querySelector('x-for') )
     };
 
     element.attributeChangedCallback = function(attr, oldVal, newVal) {
+    };
+
+    element.clear = function() {
+        var el = this;
+        while ( el.firstChild ) {
+            el.removeChild( el.firstChild );
+        };
     };
 
     var XFor = document.registerElement('x-view', {
