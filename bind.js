b65d921b2a8e89431826d1fb20f8f5c0d451f5e2
user-dob <user-dob@yandex.ua>
2014-11-20 00:07:04 +0200

    asd

--- bind.js
+++ bind.js
@@ -5,17 +5,58 @@ function bind(el) {
     var values = {};
 
     parseEl(el);
-    parseData(data, data, '');
+    parseData(data);
 
     function parseEl(el) {
         if(el.nodeType === window.Node.TEXT_NODE) {
             parseTextNode(el);
         }
 
+        if(el.nodeType === window.Node.ELEMENT_NODE) {
+            parseElementNode(el);
+        }
+
         var nodes = el.childNodes;
         for(var i=0; i<nodes.length; i++) {
             parseEl(nodes[i]);
         }
+    }
+
+    function parseElementNode(el) {
+
+        var empty = true;
+        var tpl = {};
+        var replace = function(text) {
+            text = text.replace(/[\t\n\r]*/g, '');
+            return text.replace(/\{([^{}]*)\}/g, function(math, p) {
+                values[p] = values[p] || [];
+                values[p].push(el);
+                empty = false;
+                return '"+context.' + p + '+"';
+            })
+        };
+
+        var attributes = el.attributes;
+        for(var j = 0; j<attributes.length; j++) {
+            var attr = attributes[j];
+            tpl[attr.name] = replace(attr.value);
+        };
+
+        if(!empty) {
+
+            var body = [];
+
+            for(var j = 0; j<attributes.length; j++) {
+                var attr = attributes[j];
+                body.push('el.setAttribute("' + attr.name + '", "' + tpl[attr.name] + '")');
+            };
+
+            var render = new Function('el', 'context', body.join('\n'));
+
+            el.addEventListener('bind', function(event) {
+                render(el, event.detail);
+            }, true);
+        };
     }
 
     function parseTextNode(el) {
@@ -41,22 +82,33 @@ function bind(el) {
         };
     };
 
+    function dispatchEvent(path, data) {
+        if(values[path]) {
+            values[path].forEach(function(el) {
+                el.dispatchEvent(new CustomEvent('bind', {detail: data}));
+            })
+        }
+    }
+
     function parseData(data, o, path) {
+
+        o = o || data;
+        path = path || '';
 
         Object.observe(o, function(changes) {
             changes.forEach(function(change) {
                 var name = path ? path + '.' + change.name : change.name
-                if(values[name]) {
-                    values[name].forEach(function(el) {
-                        el.dispatchEvent(new CustomEvent('bind', {detail: data}));
-                    })
-                }
+                dispatchEvent(name, data);
             })
         }, ['update']);
 
         Object.getOwnPropertyNames(o).forEach(function(name) {
+            var myPath = path ? path + '.' + name : name;
+
             if(typeof o[name] === 'object') {
-                parseData(data, o[name], path ? path + '.' + name : name);
+                parseData(data, o[name], myPath);
+            } else {
+                dispatchEvent(myPath, data);
             }
         })
     }
